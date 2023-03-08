6d2feb6c2982cab6c5b1b959798348a6c75b8c24
Avinash <asdhakne21@gmail.com>
2023-03-08 13:28:04 +0530

    database connection

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codingthunder'
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    phone_no = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
 
 @app.route("/about")
 def about():
@@ -17,6 +33,7 @@ def about():
 @app.route("/post")
 def post():
     return render_template("post.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
